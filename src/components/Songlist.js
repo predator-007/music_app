b70dc04769cbdd2de9b94985908d3eeaef918327
predator-007 <srijanraj2000@gmail.com>
2021-01-16 16:53:08 +0530

    commit

--- src/components/Songlist.js
+++ src/components/Songlist.js
@@ -1,4 +1,9 @@
-import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import Button from '@material-ui/core/Button';
+import DialogContentText from '@material-ui/core/DialogContent';
+import DialogContent from '@material-ui/core/DialogContent';
+import React, { useState } from 'react';
 const Songlist=(
 {
     songs,
@@ -15,6 +20,7 @@ const Songlist=(
         if(!isPlaying)
         setisPlaying(!isPlaying)
     }
+    const [dia,setdia]=useState(false);
     return(
         <li class={isPlaying&songs.indexOf(song)===currentsongindex
             ?"fa fa-music fa-lg list-group-item list-group-item-success":"fa fa-music fa-lg list-group-item list-group-item"}
@@ -28,7 +34,26 @@ const Songlist=(
 
         <div class="col-2">
     
-        <button className="btn fa fa-trash fa-sm" onClick={()=>deletesong(song)}></button>
+        <button className="btn fa fa-trash fa-sm" onClick={()=>setdia(!dia)}></button>
+        <Dialog
+        open={dia}
+        
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogContent>
+          <DialogContentText>
+           Do you want to delete Song {song} 
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={()=>setdia(!dia)} color="primary">
+            No
+          </Button>
+          <Button onClick={()=>{setdia(!dia);deletesong(song)}} color="primary" autoFocus>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
         </div>
         </div>
         
